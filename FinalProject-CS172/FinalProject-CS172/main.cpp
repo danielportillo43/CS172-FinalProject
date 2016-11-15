2af7af9af091d8b3de4974d6b76ba80d251c4055
Daniel Portillo <dportillo17@my.whitworth.edu>
2016-11-15 15:10:19 -0800

    mm

--- FinalProject-CS172/FinalProject-CS172/main.cpp
+++ FinalProject-CS172/FinalProject-CS172/main.cpp
@@ -10,18 +10,12 @@
 
 using namespace std;
 
-int main(){
-    cout << "Hi" << endl;
-    /*
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     
     std::cout << " help!!\n";
     return 0;
-<<<<<<< HEAD
-=======
->>>>>>> 5e30df96148d86c49b8d1d5a8496af14acc93d88
-     */
->>>>>>> e6b80c5fcca900208019b3ef0d9e2a5b83927dc1
+    //1
+
 }
