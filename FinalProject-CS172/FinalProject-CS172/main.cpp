82603acb67b5df71b708a4cddff50c7c64105da3
Daniel Portillo <dportillo17@my.whitworth.edu>
2016-11-15 14:51:49 -0800

    Initial

---
+++ FinalProject-CS172/FinalProject-CS172/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProject-CS172
+//
+//  Created by Daniel Portillo on 11/15/16.
+//  Copyright © 2016 Daniel Portillo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
